1c20e828b015703bd83df801ec032a830fb7eb7d
shinnosuke-ai7 <246853195+shinnosuke-ai7@users.noreply.github.com>
2026-05-23 13:57:20 +0530

    Update student_marks_calculator.py

--- student_marks_calculator.py
+++ student_marks_calculator.py
@@ -2,18 +2,21 @@ name=input("enter student name")
 s1=int(input("enter marks1:"))
 s2=int(input("enter marks2:"))
 s3=int(input("enter marks3:"))
-total=(s1+s2+s3)
-avg=total/3
-if avg>=90:
+if s1>100 or s2>100 or s3>100:
+       print("Invalid marks entered")
+else:       
+    total=(s1+s2+s3)
+    avg=total/3
+    if avg>=90:
        grade="A"
-elif avg>=70:
+    elif avg>=70:
        grade="B" 
-elif avg>=50:
+    elif avg>=50:
        grade="C"
-else:
+    else:
        grade="FAIL"      
-print(f"\nStudent Name: {name}")
-print(f"Total Marks: {total}")
-print(f"Average Marks: {avg:.2f}")
-print(f"Grade: {grade}")       
+    print(f"\nStudent Name: {name}")
+    print(f"Total Marks: {total}")
+    print(f"Average Marks: {avg:.2f}")
+    print(f"Grade: {grade}")       
 
